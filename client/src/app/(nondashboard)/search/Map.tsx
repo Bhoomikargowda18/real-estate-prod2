d2a44f3d19f36b912f983f3a4a1e055e38bd956d
Bhoomika <bhoomikargowda18@gmail.com>
2025-05-04 00:53:09 +0530

    first commit - auth completed

--- client/src/app/(nondashboard)/search/Map.tsx
+++ client/src/app/(nondashboard)/search/Map.tsx
@@ -22,7 +22,7 @@ const Map = () => {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
+      style: "mapbox://styles/bhoomikar/cma8jbonz00hq01s4350obba1",
       center: filters.coordinates || [-74.5, 40],
       zoom: 9,
     });
